0e9b7f6c9cd186b76c82ca83142e2c297db34ce1
liuyunfei <yunfei.liu@dreamisland.ai>
2022-07-19 21:44:57 +0800

    357

--- service/marketing/marketing-api/src/main/java/com/blue/marketing/api/model/RewardInfo.java
+++ service/marketing/marketing-api/src/main/java/com/blue/marketing/api/model/RewardInfo.java
@@ -13,56 +13,95 @@ public final class RewardInfo implements Serializable {
 
     private static final long serialVersionUID = 5642744602142762972L;
 
-    /**
-     * reward id
-     */
-    private final Long id;
+    private Long id;
+
+    private String name;
+
+    private String detail;
+
+    private String link;
 
     /**
-     * reward name
+     * reward type
+     *
+     * @see com.blue.basic.constant.marketing.RewardType
      */
-    private final String name;
+    private Integer type;
 
     /**
-     * disc
+     * reward json
      */
-    private final String detail;
+    private String data;
 
-    /**
-     * reward link
-     */
-    private final String link;
+    public RewardInfo() {
+    }
 
-    public RewardInfo(Long id, String name, String detail, String link) {
+    public RewardInfo(Long id, String name, String detail, String link, Integer type, String data) {
         this.id = id;
         this.name = name;
         this.detail = detail;
         this.link = link;
+        this.type = type;
+        this.data = data;
     }
 
     public Long getId() {
         return id;
     }
 
+    public void setId(Long id) {
+        this.id = id;
+    }
+
     public String getName() {
         return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
     }
 
     public String getDetail() {
         return detail;
     }
 
+    public void setDetail(String detail) {
+        this.detail = detail;
+    }
+
     public String getLink() {
         return link;
     }
 
+    public void setLink(String link) {
+        this.link = link;
+    }
+
+    public Integer getType() {
+        return type;
+    }
+
+    public void setType(Integer type) {
+        this.type = type;
+    }
+
+    public String getData() {
+        return data;
+    }
+
+    public void setData(String data) {
+        this.data = data;
+    }
+
     @Override
     public String toString() {
-        return "RewardDTO{" +
+        return "RewardInfo{" +
                 "id=" + id +
                 ", name='" + name + '\'' +
                 ", detail='" + detail + '\'' +
                 ", link='" + link + '\'' +
+                ", type=" + type +
+                ", data='" + data + '\'' +
                 '}';
     }
 
---
+++ service/marketing/marketing-service/src/main/java/com/blue/marketing/model/RewardCondition.java
@@ -0,0 +1,171 @@
+package com.blue.marketing.model;
+
+import com.blue.basic.model.common.SortCondition;
+import com.blue.marketing.constant.EventRecordSortAttribute;
+
+import java.io.Serializable;
+
+import static com.blue.basic.constant.common.SortType.DESC;
+
+/**
+ * event record condition for select
+ *
+ * @author liuyunfei
+ */
+@SuppressWarnings("unused")
+public final class RewardCondition extends SortCondition implements Serializable {
+
+    private static final long serialVersionUID = -8708899408485179098L;
+
+    private Long id;
+
+    private String nameLike;
+
+    private String detail;
+
+    private String link;
+
+    /**
+     * reward type
+     *
+     * @see com.blue.basic.constant.marketing.RewardType
+     */
+    private Integer type;
+
+    /**
+     * reward status
+     *
+     * @see com.blue.basic.constant.common.Status
+     */
+    private Integer status;
+
+    private Long createTimeBegin;
+
+    private Long createTimeEnd;
+
+    private Long updateTimeBegin;
+
+    private Long updateTimeEnd;
+
+    public RewardCondition() {
+        super(EventRecordSortAttribute.ID.attribute, DESC.identity);
+    }
+
+    public RewardCondition(String sortAttribute, String sortType) {
+        super(sortAttribute, sortType);
+    }
+
+    public RewardCondition(Long id, String nameLike, String detail, String link, Integer type, Integer status, Long createTimeBegin, Long createTimeEnd,
+                           Long updateTimeBegin, Long updateTimeEnd, String sortAttribute, String sortType) {
+        super(sortAttribute, sortType);
+        this.id = id;
+        this.nameLike = nameLike;
+        this.detail = detail;
+        this.link = link;
+        this.type = type;
+        this.status = status;
+        this.createTimeBegin = createTimeBegin;
+        this.createTimeEnd = createTimeEnd;
+        this.updateTimeBegin = updateTimeBegin;
+        this.updateTimeEnd = updateTimeEnd;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getNameLike() {
+        return nameLike;
+    }
+
+    public void setNameLike(String nameLike) {
+        this.nameLike = nameLike;
+    }
+
+    public String getDetail() {
+        return detail;
+    }
+
+    public void setDetail(String detail) {
+        this.detail = detail;
+    }
+
+    public String getLink() {
+        return link;
+    }
+
+    public void setLink(String link) {
+        this.link = link;
+    }
+
+    public Integer getType() {
+        return type;
+    }
+
+    public void setType(Integer type) {
+        this.type = type;
+    }
+
+    public Integer getStatus() {
+        return status;
+    }
+
+    public void setStatus(Integer status) {
+        this.status = status;
+    }
+
+    public Long getCreateTimeBegin() {
+        return createTimeBegin;
+    }
+
+    public void setCreateTimeBegin(Long createTimeBegin) {
+        this.createTimeBegin = createTimeBegin;
+    }
+
+    public Long getCreateTimeEnd() {
+        return createTimeEnd;
+    }
+
+    public void setCreateTimeEnd(Long createTimeEnd) {
+        this.createTimeEnd = createTimeEnd;
+    }
+
+    public Long getUpdateTimeBegin() {
+        return updateTimeBegin;
+    }
+
+    public void setUpdateTimeBegin(Long updateTimeBegin) {
+        this.updateTimeBegin = updateTimeBegin;
+    }
+
+    public Long getUpdateTimeEnd() {
+        return updateTimeEnd;
+    }
+
+    public void setUpdateTimeEnd(Long updateTimeEnd) {
+        this.updateTimeEnd = updateTimeEnd;
+    }
+
+    @Override
+    public String toString() {
+        return "RewardCondition{" +
+                "id=" + id +
+                ", nameLike='" + nameLike + '\'' +
+                ", detail='" + detail + '\'' +
+                ", link='" + link + '\'' +
+                ", type=" + type +
+                ", status=" + status +
+                ", createTimeBegin=" + createTimeBegin +
+                ", createTimeEnd=" + createTimeEnd +
+                ", updateTimeBegin=" + updateTimeBegin +
+                ", updateTimeEnd=" + updateTimeEnd +
+                ", sortAttribute='" + sortAttribute + '\'' +
+                ", sortType='" + sortType + '\'' +
+                '}';
+    }
+
+}
