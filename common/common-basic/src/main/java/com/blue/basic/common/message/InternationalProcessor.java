34fcc2c8a60b76f87c35ee1c10fd0238080abea8
liuyunfei <yunfei.liu@dreamisland.ai>
2022-09-08 20:53:10 +0800

    505

--- common/common-basic/src/main/java/com/blue/basic/common/message/InternationalProcessor.java
+++ common/common-basic/src/main/java/com/blue/basic/common/message/InternationalProcessor.java
@@ -12,7 +12,7 @@ import java.util.Map;
  *
  * @author liuyunfei
  */
-@SuppressWarnings({"AliControlFlowStatementWithoutBraces", "JavaDoc"})
+@SuppressWarnings({"AliControlFlowStatementWithoutBraces", "JavaDoc", "unused"})
 public final class InternationalProcessor {
 
     /**
