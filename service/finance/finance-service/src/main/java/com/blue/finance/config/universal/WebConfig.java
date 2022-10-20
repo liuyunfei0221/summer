8f977c238e331bb01a9baf209a77662179e946df
liuyunfei <yunfei.liu@dreamisland.ai>
2022-10-20 20:24:00 +0800

    535

---
+++ service/finance/finance-service/src/main/java/com/blue/finance/config/universal/WebConfig.java
@@ -0,0 +1,40 @@
+package com.blue.finance.config.universal;
+
+import org.springframework.boot.autoconfigure.http.HttpMessageConverters;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.codec.HttpMessageReader;
+import org.springframework.http.codec.HttpMessageWriter;
+import org.springframework.http.codec.ServerCodecConfigurer;
+import org.springframework.web.reactive.config.WebFluxConfigurer;
+
+import java.util.List;
+
+import static com.blue.basic.common.base.CommonFunctions.HTTP_MESSAGE_CONVERTERS;
+
+
+/**
+ * web config
+ *
+ * @author liuyunfei
+ */
+@SuppressWarnings("SpringJavaInjectionPointsAutowiringInspection")
+@Configuration
+public class WebConfig implements WebFluxConfigurer {
+
+    @Bean
+    HttpMessageConverters httpMessageConverters() {
+        return HTTP_MESSAGE_CONVERTERS;
+    }
+
+    @Bean
+    List<HttpMessageReader<?>> httpMessageReaders(ServerCodecConfigurer configurer) {
+        return configurer.getReaders();
+    }
+
+    @Bean
+    List<HttpMessageWriter<?>> httpMessageWriters(ServerCodecConfigurer configurer) {
+        return configurer.getWriters();
+    }
+
+}
